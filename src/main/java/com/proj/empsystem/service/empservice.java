96aa89c788665331695213b9c9305256dccdca85
Juiliware30 <143094155+Juiliware30@users.noreply.github.com>
2024-05-03 09:48:07 +0530

    Add files via upload

--- src/main/java/com/proj/empsystem/service/empservice.java
+++ src/main/java/com/proj/empsystem/service/empservice.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.proj.empsystem.entity.emp;
-import com.proj.empsystem.pository.emprepo;
+import com.proj.empsystem.repository.emprepo;
 
 @Service
 public class empservice {
